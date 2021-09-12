f23ca4538ee107538f9ae856f7389c5aae5d3a22
Wilgan Fauzan <wilganfauzan93@gmail.com>
2021-09-12 21:23:07 +0700

    Create Untitled-1.jsx

---
+++ Untitled-1.jsx
@@ -0,0 +1,31 @@
+//-------------------------
+//Javascript101
+//-------------------------
+
+
+console.log("One Small Step to Success")
+
+let firstname = "Wilgan";
+let lastname = "Fauzan";
+let sayhello = `Hello bisa kan ${firstname}`;
+
+let fullname = firstname + " " + lastname;
+
+let name = "Wilgan Fauzan Prabadhi";
+let middlename = name.substring(7, 14)
+
+let namenumber = "2601wilgan";
+let number = namenumber.substring(0, 2)
+
+let nomber = 1001;
+let numbertostring = nomber.toString()
+
+console.log(firstname)
+console.log(sayhello)
+console.log(firstname[3])
+console.log(fullname)
+console.log(fullname.length)
+console.log(middlename)
+console.log(parseInt(namenumber) + 10)
+console.log(numbertostring)
+console.log(numbertostring + 200)
